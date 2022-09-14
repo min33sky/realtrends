41ea57e85f02652db0b13ff3d440030563cecf96
min33sky <min33sky@naver.com>
2022-09-15 00:07:10 +0900

    feat: implements item removal

--- client/app/components/items/ItemViewer.tsx
+++ client/app/components/items/ItemViewer.tsx
@@ -1,3 +1,4 @@
+import { Link } from '@remix-run/react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useUser } from '~/contexts/UserContext';
 import { useDateDistance } from '~/hooks/useDateDistance';
@@ -40,33 +41,40 @@ export default function ItemViewer({ item }: Props) {
     }
   };
 
+  console.log('item link: ', item.link);
+
   return (
     <article className="flex flex-col">
       <figure>
         {thumbnail ? (
-          <img
-            className="h-auto max-h-[80vh] w-full object-contain"
-            src={thumbnail}
-            alt="thumbnail"
-          />
+          <a href={item.link} className="block">
+            <img
+              className="h-auto max-h-[80vh] w-full object-contain"
+              src={thumbnail}
+              alt="thumbnail"
+            />
+          </a>
         ) : null}
       </figure>
+
       <div aria-label="content" className="border-b border-gray-100 p-4">
-        <div aria-label="publisher" className="mb-2 flex items-center">
-          {publisher.favicon ? (
-            <img
-              src={publisher.favicon}
-              className="mr-2 h-4 w-4"
-              alt="favicon"
-            />
-          ) : (
-            <Globe className="mr-2" />
-          )}
-          <p>{author ? `${author} · ` : ''}</p>
-          <p className="ml-1 text-sm text-gray-600">{publisher.name}</p>
-        </div>
-        <h3 className="mb-2 font-semibold text-gray-800">{title}</h3>
-        <p className="mb-2 text-sm text-gray-500 line-clamp-5">{body}</p>
+        <a href={item.link} className="block">
+          <div aria-label="publisher" className="mb-2 flex items-center">
+            {publisher.favicon ? (
+              <img
+                src={publisher.favicon}
+                className="mr-2 h-4 w-4"
+                alt="favicon"
+              />
+            ) : (
+              <Globe className="mr-2" />
+            )}
+            <p>{author ? `${author} · ` : ''}</p>
+            <p className="ml-1 text-sm text-gray-600">{publisher.name}</p>
+          </div>
+          <h3 className="mb-2 font-semibold text-gray-800">{title}</h3>
+          <p className="mb-2 text-sm text-gray-500 line-clamp-5">{body}</p>
+        </a>
 
         <AnimatePresence initial={false}>
           {likes === 0 ? null : (
