1ee07e50fbbd67b723f09665ab62ec8cea7b3844
min33sky <min33sky@naver.com>
2022-09-23 03:35:47 +0900

    feat: create ranking bot

--- server/src/workers/ranking.ts
+++ server/src/workers/ranking.ts
@@ -1,16 +1,23 @@
 import PQueue from 'p-queue';
 import db from '../lib/db';
 import { calcurateRankingScore } from '../lib/ranking';
+import cron from 'node-cron';
 
 /**
- *? 점수가 0.001보다 낮은 아이템들에 대해서 점수 계산을 다시 해야함
+ *? 오래된 아이템일수록 점수가 0에 가까워진다.
+ *? 점수가 0.001보다 낮은 아이템들에 대해서 좋아요수를 기반으로
+ *? 점수 계산을 다시 해야함
+ */
+
+/**
+ * 0.001보다 낮은 점수를 가진 아이템들을 찾는 함수
  */
 async function findRecalculateTargets() {
   const data = await db.itemStats.findMany({
     where: {
-      score: {
-        lte: 0.001,
-      },
+      // score: {
+      //   lte: 0.001,
+      // },
     },
     select: {
       itemId: true,
@@ -30,6 +37,9 @@ async function recalculate() {
   const queue = new PQueue({ concurrency: 10 });
   const targets = await findRecalculateTargets();
   const now = Date.now();
+
+  console.log(`Recalculating ${targets.length} items`);
+
   targets.forEach((itemStat) => {
     queue.add(async () => {
       const hourAge =
@@ -47,12 +57,11 @@ async function recalculate() {
     });
   });
 
-  return queue.onIdle();
+  await queue.onIdle();
+  console.log(`Recalculating successfully`);
 }
 
-function main() {
-  //? 1. 점수가 0.001보다 낮은 아이템들을 찾는다.
-  //? 2. 점수를 다시 계산한다.
-  //? 3. 점수를 업데이트한다.
-  // register crons
-}
+//? 1. 점수가 0.001보다 낮은 아이템들을 찾는다.
+//? 2. 점수를 다시 계산한다.
+//? 3. 점수를 업데이트한다.
+cron.schedule('*/5 * * * *', recalculate);
