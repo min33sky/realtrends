f46c8f5bedaf9722e34021ab3fe5a078b0aaa60d
min33sky <min33sky@naver.com>
2022-09-26 05:50:31 +0900

    fix: navigate to home when pressed logo

--- client/app/components/base/DesktopHeader.tsx
+++ client/app/components/base/DesktopHeader.tsx
@@ -1,3 +1,4 @@
+import { Link } from '@remix-run/react';
 import React from 'react';
 import Button from '../system/Button';
 import { Logo } from '../vectors';
@@ -6,7 +7,9 @@ import SearchArea from './SearchArea';
 export default function DesktopHeader() {
   return (
     <header className="hidden h-16 items-center border-b border-gray-500 px-4 xs:flex">
-      <Logo className="block h-[17px] w-auto" />
+      <Link to="/">
+        <Logo className="block h-[17px] w-auto" />
+      </Link>
       <div className="flex flex-1 items-center justify-between">
         <div className="flex items-center"></div>
         <div className="flex items-center">
